50dbd6b2fce19d44f5168c25cf53fadeb71fd0b3
uupt <270628297@qq.com>
2025-06-13 15:39:47 +0800

    Update: main.js --> main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,16 @@ import './assets/main.css'
 
 import {createApp} from 'vue'
 import App from './App.vue'
-import {createRouter, createWebHashHistory, createWebHistory} from "vue-router";
+import {
+    createRouter,
+    createWebHashHistory,
+    RouteRecordRaw
+} from "vue-router";
 import HomePage from "@/pages/HomePage.vue";
 import AboutPage from "@/pages/AboutPage.vue";
 import NewsPage from "@/pages/NewsPage.vue";
 
-const routes = [
+const routes: RouteRecordRaw[] = [
     // 默认跳转都⾸⻚
     {path: '/', redirect: '/home'},
     {path: '/home', component: HomePage},
@@ -17,7 +21,10 @@ const routes = [
 ]
 
 const router = createRouter({
+    // 访问路径：URL不带#，斜杠链接，接近传统⽹站。缺点：容易产⽣404错误。
     // history: createWebHistory(),
+
+    // 访问路径：URL带有#。缺点：对SEO不太友好。⽐较适合内部系统。
     history: createWebHashHistory(),
     routes,
 })
