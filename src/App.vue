6f7f49cb3938c013caf0a4b98e31eb7e7e1f47ca
uupt <270628297@qq.com>
2025-06-13 15:26:02 +0800

    Add: vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import MySalaryInfo from "@/components/MySalaryInfo.vue";
-import {reactive, ref} from "vue";
+import {reactive} from "vue";
 
 const salaryInfo = reactive({
   username: "张三",
@@ -20,12 +19,38 @@ function addSalary() {
 </script>
 
 <template>
+<!--  <div>-->
+<!--    <MySalaryInfo :salary-info="salaryInfo"/>-->
+<!--    <button @click="addSalary">增加薪水</button>-->
+<!--    <button @click="showRes">查看薪水</button> {{ salaryInfo }}-->
+<!--  </div>-->
   <div>
-    <MySalaryInfo :salary-info="salaryInfo"/>
-    <button @click="addSalary">增加薪水</button>
-    <button @click="showRes">查看薪水</button> {{ salaryInfo }}
+    <h2>1</h2>
+    <p>
+      <!-- 直接跳转 -->
+      <RouterLink to="/home">⾸⻚</RouterLink>
+      <!-- 命名跳转 -->
+      <RouterLink :to="{ name:'about'}">关于</RouterLink>
+      <!-- 路径跳转 -->
+      <RouterLink replace :to="{ path:'/news'}">新闻</RouterLink>
+    </p>
+    <div class="content">
+      <!-- 路由出⼝ -->
+      <!-- 路由匹配到的组件将渲染在这⾥ -->
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <style scoped>
+a {
+  margin: 10px;
+}
+
+.content {
+  background: yellowgreen;
+  height: 400px;
+  width: 400px;
+  border-radius: 10px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
+import {createRouter, createWebHashHistory, createWebHistory} from "vue-router";
+import HomePage from "@/pages/HomePage.vue";
+import AboutPage from "@/pages/AboutPage.vue";
+import NewsPage from "@/pages/NewsPage.vue";
 
-createApp(App).mount('#app')
+const routes = [
+    // 默认跳转都⾸⻚
+    {path: '/', redirect: '/home'},
+    {path: '/home', component: HomePage},
+    // 命名路由
+    {path: '/about', component: AboutPage, name: 'about'},
+    {path: '/news', component: NewsPage},
+]
+
+const router = createRouter({
+    // history: createWebHistory(),
+    history: createWebHashHistory(),
+    routes,
+})
+
+createApp(App)
+    .use(router)
+    .mount('#app')
