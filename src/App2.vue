3a32e1e805659e7610f2247a4bf7a83923162b1e
uupt <270628297@qq.com>
2025-10-14 15:01:18 +0800

    Add: 编写App组件

---
+++ src/App2.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h2>Hello World</h2>
+</template>
+
+<style scoped>
+.app {
+  background-color: #ddd;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+</style>
